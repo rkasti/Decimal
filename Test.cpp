217d4c716c3ddc4aac7f0ddc10573e5e57e7474a
Raphael <raphael.kastenmeier@gmail.com>
2024-01-06 14:19:56 +0100

    save before changing multiplication... again...

--- Test.cpp
+++ Test.cpp
@@ -8,18 +8,19 @@
 #include <vector>
 
 
+// 770 cycles on average
 int main()
 {
-	Decimal a(3175, -1);
-	Decimal b(5, -1);
+	Decimal a(121212121212, 0);
+	Decimal b(121212121212, 0);
 	Decimal c;
-	c = a * b;
-	std::cout << c.to_string() << std::endl;
-	// auto start = std::chrono::system_clock::now();
-	// for (size_t counter = 0; counter < 1000000; ++counter) {
-	//	c = a + b;
-	// }
-	// auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now() - start);
-	// std::cout << duration.count() / 1000000 * 4.3 << " cycles on average" << std::endl;
-	// std::cout << "result: " << c.to_string() << std::endl;
+	// c = a * b;
+	// std::cout << c.to_string() << std::endl;
+	auto start = std::chrono::system_clock::now();
+	for (size_t counter = 0; counter < 1000000; ++counter) {
+		c = a * b;
+	}
+	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now() - start);
+	std::cout << duration.count() / 1000000 * 4.3 << " cycles on average" << std::endl;
+	std::cout << "result: " << c.to_string() << std::endl;
 }
