a96adc4a226831087d340497a8440f455bf78581
Raphael <raphael.kastenmeier@gmail.com>
2024-01-08 15:32:38 +0100

    renamed project to Decimal

--- Test.cpp
+++ Test.cpp
@@ -1,14 +1,9 @@
 #include "Decimal.h"
-#include <algorithm>
-#include <bitset>
 #include <chrono>
 #include <iostream>
-#include <math.h>
 #include <string>
-#include <vector>
 
 
-// 200 / 770 cycles on average
 int main()
 {
 	Decimal a(-456, 3);
@@ -16,11 +11,4 @@ int main()
 	Decimal c;
 	c = a % b;
 	std::cout << c.to_string() << std::endl;
-	// auto start = std::chrono::system_clock::now();
-	// for (size_t counter = 0; counter < 1000000; ++counter) {
-	//	c = a * b;
-	// }
-	// auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now() - start);
-	// std::cout << duration.count() / 1000000 * 4.3 << " cycles on average" << std::endl;
-	// std::cout << "result: " << c.to_string() << std::endl;
 }
